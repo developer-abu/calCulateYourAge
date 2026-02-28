f997469aa72e4ffe8a6dada05bacf673e1a91d0c
developer-abu <officialworkofhurairashaikh01@gmail.com>
2026-02-28 11:06:27 +0530

    all the necessary js for this writing js is doen

--- script.js
+++ script.js
@@ -9,26 +9,58 @@ let showResult = document.getElementById("showResult");
 
 
 
-submitBTN.addEventListener("click", (e)=>{
-    e.preventDefault()
-    //value of date input fields 
-let dobInputValue = dobInput.value;
-let cDateInputValue = cDateInput.value;
-// slicing and transforming into number dob date year month
-let dobYear = parseInt(dobInputValue.slice(0,4));
-let dobMonth = parseInt(dobInputValue.slice(5,7));
-let dobDate = parseInt(dobInputValue.slice(8,9));
-//slicing and transforming into number current date year month
-let cYear = parseInt(cDateInputValue.slice(0,4));
-let cMonth = parseInt(cDateInputValue.slice(5,7));
-let cDate = parseInt(cDateInputValue.slice(8,9));
-// printing in console
-console.log(dobYear);
-console.log(dobMonth);
-console.log(dobDate);
-// printing in console
-console.log(cYear);
-console.log(cMonth);
-console.log(cDate);
 
-})
+submitBTN.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    let dobInputValue = dobInput.value;
+    let cDateInputValue = cDateInput.value;
+
+    let dobYear = parseInt(dobInputValue.slice(0, 4));
+    let dobMonth = parseInt(dobInputValue.slice(5, 7));
+    let dobDate = parseInt(dobInputValue.slice(8, 10));
+
+    let cYear = parseInt(cDateInputValue.slice(0, 4));
+    let cMonth = parseInt(cDateInputValue.slice(5, 7));
+    let cDate = parseInt(cDateInputValue.slice(8, 10));
+
+    let year = cYear - dobYear;
+    let month = cMonth - dobMonth;
+    let date = cDate - dobDate;
+
+
+    if (month >= 0 && date >= 0) {
+     showResult.innerHTML = `Age: ${year} Month: ${month} Days: ${date}`
+    }
+
+
+    else if (month < 0 && date >= 0) {
+        year--;
+        month += 12;
+
+       showResult.innerHTML = `Age: ${year} Month: ${month} Days: ${date}`
+    }
+
+
+    else if (month >= 0 && date < 0) {
+
+        let daysInPrevMonth = new Date(cYear, cMonth - 1, 0).getDate();
+        date += daysInPrevMonth;
+        month--;
+
+showResult.innerHTML = `Age: ${year} Month: ${month} Days: ${date}`
+    }
+
+
+    else if (month < 0 && date < 0) {
+
+        let daysInPrevMonth = new Date(cYear, cMonth - 1, 0).getDate();
+        date += daysInPrevMonth;
+
+        month--;     
+        year--;  
+        month += 12;
+
+  showResult.innerHTML = `Age: ${year} Month: ${month} Days: ${date}`
+    }
+});
